4c23f58d8f17f3bbb23b4443928f0e3d457c3a44
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-07-06 18:18:30 +0200

    feat: Added first working code

---
+++ app/database.py
@@ -0,0 +1,19 @@
+# database.py
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost/dbname"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
